920a048a3c70679ce85d3715a885f314dd9eb163
irazum <conscientiaaa@gmail.com>
2021-03-07 20:54:05 +0300

    task_manage v0.09

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('task-list/', views.taskList, name="task-list")
+    path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<int:pk>/', views.taskDetail, name="task-detail"),
+    path('task-create/', views.taskCreate, name="task-create"),
+    path('task-update/<int:pk>/', views.taskUpdate, name="task-update"),
+    path('task-delete/<int:pk>/', views.taskDelete, name="task-delete")
 ]
